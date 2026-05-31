cookieguru/afieldguidetoelephpants

commit a709efc04777ef0275401abd7bd91789119cc2f9

Upgrade to PHP 8.0

src/JsonBundle/DynamicJsonSource.php
@@ -26,7 +26,7 @@ class DynamicJsonSource extends AbstractSource
     /**
      * @param array|mixed $content Content to render as JSON
      */
-    public function setContent($content = null)
+    public function setContent(?string $content = null): void
     {
         parent::setContent($content);
         $this->setFormattedContent(json_encode($content, JSON_PRETTY_PRINT));

app/ElephpantTwigExtension.php
@@ -1,12 +1,15 @@
 <?php
 
-class ElephpantTwigExtension extends Twig_Extension
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class ElephpantTwigExtension extends AbstractExtension
 {
     public function getFilters()
     {
         $filters = array(
-             new Twig_SimpleFilter('merge_recursive', [$this, 'filterMergeRecursive']),
-             new Twig_SimpleFilter('sortby', [$this, 'filterSortBy']),
+             new TwigFilter('merge_recursive', [$this, 'filterMergeRecursive']),
+             new TwigFilter('sortby', [$this, 'filterSortBy']),
         );
         return $filters;
     }

app/SculpinKernel.php
@@ -4,7 +4,7 @@ require_once 'ElephpantTwigExtension.php';
 
 class SculpinKernel extends \Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel
 {
-    protected function getAdditionalSculpinBundles()
+    protected function getAdditionalSculpinBundles() : array
     {
         return ['AFieldGuideToElephpants\\JsonBundle\\JsonBundle'];
     }

commit 2999686b58caa25d4976618dff926c865cfb9d8d

Fix generation of JSON feed
Likely broke in a709efc04777ef0275401abd7bd91789119cc2f9

src/JsonBundle/DependencyInjection/JsonCompilerPass.php
@@ -1,0 +1,22 @@
+<?php
+
+namespace AFieldGuideToElephpants\JsonBundle\DependencyInjection;
+
+use AFieldGuideToElephpants\JsonBundle\JsonBuilder;
+use Sculpin\Core\Sculpin;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class JsonCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $container
+            ->getDefinition('event_dispatcher')
+            ->addMethodCall('addListener', [
+                Sculpin::EVENT_AFTER_FORMAT,
+                [new Reference(JsonBuilder::BUNDLE_NAME), 'onSculpinCoreAfterFormat']
+            ]);
+    }
+}

src/JsonBundle/DependencyInjection/JsonExtension.php
@@ -1,0 +1,22 @@
+<?php
+
+namespace AFieldGuideToElephpants\JsonBundle\DependencyInjection;
+
+use AFieldGuideToElephpants\JsonBundle\JsonBuilder;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Reference;
+
+class JsonExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $container
+            ->register(JsonBuilder::BUNDLE_NAME, JsonBuilder::class)
+            ->addArgument(new Reference('sculpin.site_configuration'))
+            ->addTag('kernel.event_listener', [
+                'event' => 'sculpin.core.after_format',
+                'method' => 'onSculpinCoreAfterFormat',
+            ]);
+    }
+}

src/JsonBundle/DynamicJsonSource.php
@@ -14,7 +14,7 @@ class DynamicJsonSource extends AbstractSource
     /**
      * @param string $filename Desired name of the file; used for the permalink
      */
-    public function __construct($filename)
+    public function __construct(string $filename)
     {
         $this->data = new Data();
         $this->isGenerated = true;
@@ -24,12 +24,12 @@ class DynamicJsonSource extends AbstractSource
     }
 
     /**
-     * @param array|mixed $content Content to render as JSON
+     * @param ?string $content Content to render as JSON
      */
     public function setContent(?string $content = null): void
     {
         parent::setContent($content);
-        $this->setFormattedContent(json_encode($content, JSON_PRETTY_PRINT));
+        $this->setFormattedContent($content);
 
         $this->hasChanged = true;
     }

src/JsonBundle/JsonBuilder.php
@@ -9,6 +9,8 @@ use Sculpin\Core\Source\SourceInterface;
 
 class JsonBuilder extends AbstractSource
 {
+    public const BUNDLE_NAME = 'json.builder';
+
     private $config;
 
     public function __construct(Configuration $config)
@@ -31,7 +33,7 @@ class JsonBuilder extends AbstractSource
 
         // Create new JSON file with the elephpant data
         $json = new DynamicJsonSource('data/all.json');
-        $json->setContent($elephpants);
+        $json->setContent(json_encode($elephpants, JSON_PRETTY_PRINT));
         $event->sourceSet()->mergeSource($json);
     }
 

src/JsonBundle/JsonBundle.php
@@ -2,17 +2,14 @@
 
 namespace AFieldGuideToElephpants\JsonBundle;
 
+use AFieldGuideToElephpants\JsonBundle\DependencyInjection\JsonCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JsonBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        $container
-            ->register('json.builder', 'AFieldGuideToElephpants\JsonBundle\JsonBuilder')
-            ->addArgument(new Reference('sculpin.site_configuration'))
-            ->addTag('kernel.event_listener', array('event' => 'sculpin.core.after_format'));
+        $container->addCompilerPass(new JsonCompilerPass());
     }
 }